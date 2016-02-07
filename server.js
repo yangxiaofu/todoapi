5e2243736b4ee0a5e07f34e74cf6ffdc136799df
Dave Dong <fudaviddong@Dave-Dongs-Mac-Mini.local>
2016-02-07 09:45:49 -0800

    started server

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+	res.send('todo API root');
+});
+
+app.listen(PORT, function(){
+	console.log('Express Listening on port ' + PORT + '!');
+});
