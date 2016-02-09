08cbf062f9e60b55156bcbba8758a2be2d19f27c
Dave Dong <fudaviddong@Dave-Dongs-Mac-Mini.local>
2016-02-08 22:09:30 -0800

    Add DELETE todos route

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 var express = require('express');
-
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 
 var app = express();
-
 var PORT = process.env.PORT || 3000;
-
 var todos = [];
-
 var todoNextId = 1;
+
 
 app.get('/', function(req, res){
 	res.send('todo API root');
@@ -21,17 +20,12 @@ app.get('/todos', function(req, res){
 	res.json(todos);
 });
 
+
 //GET /todos/:id
 app.get('/todos/:id', function(req, res){
 	var todoId = parseInt(req.params.id, 10);  //always use ten here. 
 
-	var matchedTodo;
-
-	todos.forEach(function(todo){
-		if (todoId === todo.id){
-			matchedTodo = todo;
-		}
-	});
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
 	if (matchedTodo){
 		res.json(matchedTodo);
@@ -40,15 +34,39 @@ app.get('/todos/:id', function(req, res){
 	}
 });
 
+
+
 //POST request /todos
 app.post('/todos', function(req, res){
 	var body = req.body;
 
+	if (!_.isBoolean(body.completed) || (!_.isString(body.description)) || (body.description.trim()).length === 0) {
+		return res.status(400).send();
+	}
+
+	// add id field
 	body.id = todoNextId++;
 
+	//push body into array
 	todos.push(body);
 
 	res.json(body);
+});
+
+// DELETE / todos/:id
+app.delete('/todos/:id', function(req, res){
+	
+	var todoId = parseInt(req.params.id, 10);
+
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if (!matchedTodo) {
+		res.status(404).json({"error": todoId});
+	} else {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
+
 });
 
 
