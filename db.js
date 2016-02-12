d76b94b652d82a3022a535a6ae092ec57cbb4853
Dave Dong <fudaviddong@Dave-Dongs-Mac-Mini.local>
2016-02-11 22:54:47 -0800

    Add todo model

---
+++ db.js
@@ -0,0 +1,13 @@
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize(undefined, undefined, undefined, {
+	'dialect': 'sqlite',
+	'storage': __dirname + '/data/dev-todo-api.sqlite'
+});
+
+var db = {};
+db.todo = sequelize.import(__dirname + '/models/todos.js');
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
